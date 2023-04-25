b056483c58a756d4ac7485e551d92d9068a577f2
DESKTOP-E9KBGN3\AOF <off_17212@hotmail.com>
2023-04-25 16:57:50 +0700

    WorkShop #1 (Limit Message)

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2 align="center">Workshop #1 - Limit Message</h2>
+    <hr />
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" v-text="max - message.length"></span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        v-model="message"
+        :maxlength="max"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      message: "",
+      max: 20,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
